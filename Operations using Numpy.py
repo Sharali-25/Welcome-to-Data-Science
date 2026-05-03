e623a0039bb9be095f39f26fee1f3fa6ea441f6a
Sharali-25 <Sharaliarkansas@gmail.com>
2026-05-03 10:28:19 -0500

    Add files via upload

---
+++ Operations using Numpy.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+a=np.array([35,67,85])
+print('First Array:')
+print(a)
+print('\n')
+
+b=np.array([10,10,10])
+print('Second Array')
+print(b)
+print('\n')
+
+print('Add the two arrays:')
+print(np.add(a,b))
+print('\n')
+
+print('Subtract the two arrays:')
+print(np.subtract(a,b))
+print('\n')
+
+print('Multiply the two arrays:')
+print(np.multiply(a,b))
+print('\n')
+
+print('Division the two arrays:')
+print(np.divide(a,b))
+print('\n')
